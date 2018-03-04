1d0598263e232728afe2df750787b966aee371b3
mithunmanohar <mithunmanohar79@gmail.com>
2018-03-04 20:34:47 +0530

    fixed formatting

--- application.py
+++ application.py
@@ -1,32 +1,32 @@
 import json
 import requests
+import pymongo
+import pandas as pd
 from flask import Flask
 from flask import request
 from bs4 import BeautifulSoup
-import pandas as pd
-import pymongo
 from pymongo import MongoClient
+
 app = Flask(__name__)
 
-
-@app.route('/')
-def btc():
-    return json.dumps({"#":"1", "name":"BTC Bitcoin", "price":"$11260.20", "Volume (24h)":"$7,689,590,000", "Market Cap":"$190,012,350,557", "Symbol": "BTC"})
 
 def get_db():
     client = MongoClient('localhost:27017')
     db = client.cmcdata
     return db
 
+@app.route('/')
+def status():
+    return json.dumps({"status":"Success"})
+
 @app.route('/get_data')
 def get_data():
     coin = request.args.get('coin_name')
-    #coin_List = coin.split(",")
     auth_key = request.args.get('auth_key')
     db = get_db()
     if auth_key != "fdsrtw435s6af8dsd9sa":
         return "{ACCESS DENIED:Authentication Failed}"
-    if coin: 
+    if coin:
         coin = coin.split(",")
         data = db.cryptodata.find({'name':coin[0]})
     else:
@@ -37,43 +37,38 @@ def get_data():
             ret_data.append(dts)
     return json.dumps(ret_data)
 
-@app.route('/get_coin_list') 
-def get_coin_list(): 
-    db = get_db() 
-    data = db.cryptodata.find({}) 
+
+@app.route('/get_coin_list')
+def get_coin_list():
+    db = get_db()
+    data = db.cryptodata.find({})
     coins = {}
-    a = []
-    for each in data: 
-        for cn in each['data']: 
-            #print cn
-            #return json.dumps(cn['org_name']) 
-            try: 
-                #coin_name = cn['org_name'] 
-                a.append({cn['org_name']: cn['ticker']})
-            except Exception as e: 
-                #print e
-                #return e
-                pass 
-    
-    coins["data"] = a
-    
+    coin_data = []
+    for each in data:
+        for cn in each['data']:
+            try:
+                coin_data.append({cn['org_name']: cn['ticker']})
+            except Exception as e:
+                pass
+
+    coins["data"] = coin_data
+
     return json.dumps(coins)
 
 @app.route('/get_coins')
 def get_coins():
-    url = "https://coinmarketcap.com/all/views/all/" 
-    html = requests.get(url).text 
-    soup = BeautifulSoup(html, 'lxml') # pass the html to lxml parser 
-    table = soup.find_all('table')[0] # find table from html 
-    df = pd.read_html(str(table)) 
-    k = (df[0].to_json(orient='records')) # 
-    data = json.loads(k) 
+    url = "https://coinmarketcap.com/all/views/all/"
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, 'lxml')
+    table = soup.find_all('table')[0]
+    df = pd.read_html(str(table))
+    k = (df[0].to_json(orient='records'))
+    data = json.loads(k)
     coin_list = []
-    for rec in data: 
+    for rec in data:
         coin_list.append({"coin":rec['Name'], "ticker":rec['Symbol']})
-
     return json.dumps({"coins":coin_list})
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",  debug=True)
+    app.run(host="0.0.0.0", debug=True)
--- cmc_scrapper_1_0.py
+++ cmc_scrapper_1_0.py
@@ -1,44 +1,35 @@
-__author__ = "samrohn77@gmail.com"
-
-from bs4 import BeautifulSoup
-import pandas as pd
-from pymongo import MongoClient
-import requests
 import json
-import datetime
 import pytz
+import requests
+import datetime
+import pandas as pd
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
 
 def get_db():
     client = MongoClient('localhost:27017')
-    db = client.cmcdata
+    db = client.cmcdata # database : cmcdata, collection:cryptodata
     return db
-
-def update_db(data):
-    pass
 
 def get_cmc_data():
     url = "https://coinmarketcap.com/all/views/all/"
     html = requests.get(url).text
-
-    soup = BeautifulSoup(html, 'lxml') # pass the html to lxml parser
-    table = soup.find_all('table')[0]  # find table from html
-    df = pd.read_html(str(table))      
-    k = (df[0].to_json(orient='records')) # 
+    soup = BeautifulSoup(html, 'lxml')
+    table = soup.find_all('table')[0]
+    df = pd.read_html(str(table))
+    k = (df[0].to_json(orient='records'))
     data = json.loads(k)
     return data
 
 def insert_data(db, data):
     now = datetime.datetime.now()
-    date_tod = now.strftime("%Y-%m-%d")
+    date_today = now.strftime("%Y-%m-%d")
     for rec in data:
         coin = rec['Name']
-        print coin
-        #continue
         res = db.cryptodata.find({"name" : coin})
-        
         if res.count() > 0:
             up_data = {}
-            up_data['date'] = date_tod
+            up_data['date'] = date_today
             up_data['rank'] = rec['#']
             up_data['ticker'] = rec['Symbol']
             up_data['price'] = rec['Price']
@@ -48,23 +39,14 @@ def insert_data(db, data):
             up_data['market_cap'] = rec['Market Cap']
             up_data['% 1hr'] = rec['% 1h']
             up_data['ciculating_supply'] = rec['Circulating Supply']
-            print 'coin already exists.Updating'
             db.cryptodata.update({"name" : coin},{'$push':{'data': up_data}})
-            
         else:
-            print 'inserting coin, ', coin
             record = {}
             db.cryptodata
             record['name'] = coin
             record['data'] = []
             details = {}
-            now = datetime.datetime.now()
-            cet_tz = pytz.timezone('CET')
-            #date_tod = now.strftime("%Y-%m-%d")
-            #date_tod = cet_tz.localize(now)
-            date_tod = now.strftime("%Y-%m-%d")
-            #print date_tod
-            details['date'] = date_tod
+            details['date'] = date_today
             details['rank'] = rec['#']
             details['ticker'] = rec['Symbol']
             details['price'] = rec['Price']
@@ -72,23 +54,13 @@ def insert_data(db, data):
             details['Volume (24h)'] = rec['Volume (24h)']
             details['% 24h'] = rec['% 24h']
             details['market_cap'] = rec['Market Cap']
-
             details['% 1h'] = rec['% 1h']
             details['circulating_supply'] = rec['Circulating Supply']
-
             record['data'].append(details)
-            print '-- ', json.dumps(record)
             db.cryptodata.insert(record)
 
-            #print json.dumps(record)
-        
 
 if __name__ == '__main__':
     data = get_cmc_data()
     db = get_db()
-    #print db.cmcdata.collection
-    #clc = db.cryptodata
-    #res =  db.cryptodata.find({})
-    #for each in data:
-    #    print each
     insert_data(db, data)
